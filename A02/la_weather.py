a51a75fbc3464b0d41d3e97ad18584583d395aaa
louisestella <louiselbarbosa@gmail.com>
2018-03-26 21:29:56 -0300

    la-weather warming up exercise

---
+++ A02/la_weather.py
@@ -0,0 +1,19 @@
+#la_Weather.py - warming up exercise
+
+weather_data = []
+f = open("la_weather.csv", 'r')
+data = f.read()
+rows = data.split('\n')
+for row in rows:
+    split_row = row.split(",")
+    weather_data.append( split_row[1] )
+weather_data = weather_data[1:]
+
+weather_counts = {}
+for item in weather_data:
+    if item in weather_counts:
+        weather_counts[item] += 1
+    else:
+        weather_counts[item] = 1
+        
+print(weather_counts)
